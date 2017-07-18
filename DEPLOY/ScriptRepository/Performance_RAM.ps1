450d7feeb28640ab2e81aada683fdec0696eb517
Mikel V <micelshima@users.noreply.github.com>
2017-07-18 09:06:25 +0200

    Add files via upload

--- DEPLOY/ScriptRepository/Performance_RAM.ps1
+++ DEPLOY/ScriptRepository/Performance_RAM.ps1
@@ -1,6 +1,6 @@
 $scriptbasename=$MyInvocation.Mycommand.name.substring(0,$MyInvocation.Mycommand.name.lastindexof('.'))
 $logfile= "results\$($scriptbasename).csv"
-if (!(test-path $logfile)){out-file $logfile -input "Computername	PercentUsed	AvailableMB"}
+if (!(test-path $logfile)){out-file $logfile -input "COMPUTERNAME	SIZEMB	AVAILABLEMB	USEDMB	PERCENTUSED"}
 try{
 	if($scope -eq ''){$result=gwmi -computername $computername Win32_PerfRawData_PerfOS_Memory -ea stop}
 	else{$result=gwmi -computername $computername Win32_PerfRawData_PerfOS_Memory -credential $creds -ea stop}
@@ -10,7 +10,7 @@ try{
 	$totalMB=$result2.TotalPhysicalMemory/1MB
 	$UsedMB=$totalMB - $FreeMB
 	Append-Richtextbox -ComputerName $computername -Source $scriptbasename -Message 'RAM retrieved successfully' -MessageColor 'blue' -logfile 'ps1command.log'
-	$line='{0}	{1:P}	{2}' -f $computername,($UsedMB/$totalMB),$FreeMB
+	$line='{0}	{1:N0}	{2:N0}	{3:N0}	{4:P}' -f $computername,$totalMB,$FreeMB,$UsedMB,($UsedMB/$totalMB)
 	out-file $logfile -input $line -append
 	}
 	catch{Append-Richtextbox -ComputerName $computername -Source $scriptbasename -Message $_.Exception.Message -MessageColor 'red' -logfile 'ps1command.log'}
