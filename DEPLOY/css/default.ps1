76e31a5a3e7c36a73025c27369f508876cc83796
Mikel V <micelshima@users.noreply.github.com>
2017-12-07 18:09:01 +0100

    new styles

---
+++ DEPLOY/css/default.ps1
@@ -0,0 +1,16 @@
+$css = [pscustomobject]@{
+	textcolor             = [System.Drawing.Color]::WhiteSmoke
+	formcolor             = [System.Drawing.Color]::Teal
+	tabcolor              = [System.Drawing.Color]::WhiteSmoke
+	DeployButton          = [System.Drawing.Color]::PaleGreen
+	richcolor             = [System.Drawing.Color]::MidnightBlue
+	richtextcolor1        = 'white'
+	richtextcolor2        = 'DeepskyBlue'
+	richtextcolorOK       = 'lime'
+	richtextcolorERR      = 'OrangeRed'
+	richtextcolorcomputer = 'lightsalmon'
+	richtextcolorwarning  = 'yellow'
+	textfont              = new-object System.Drawing.Font("Lucida Console", 10)
+	smallfont             = new-object System.Drawing.Font("Calibri", 8)
+	richtextfont          = new-object System.Drawing.Font("Lucida Console", 9)
+}
